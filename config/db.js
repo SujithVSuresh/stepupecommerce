5b64c596474be2496dea291200e188f69339e4b5
SujithVSuresh <sujitheternal13@gmail.com>
2024-08-10 12:44:11 +0530

    removed unwanted fields from mongodb connection

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const dbUrl = process.env.DB_URL;
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(dbUrl)
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('MongoDB connection error:', error));
 
