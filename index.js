d503ed13d5d77e132ecd726461c80092b6286a40
SujithVSuresh <sujitheternal13@gmail.com>
2025-02-24 18:42:34 +0530

    changed the node env logic in index"

--- index.js
+++ index.js
@@ -106,11 +106,11 @@ app.get('*', (req, res) => {
 //   console.log("Server has started");
 // });
 
-if (process.env.NODE_ENV !== "production") {
+// if (process.env.NODE_ENV !== "production") {
   const port = process.env.PORT || 5000;
   app.listen(port, () => {
     console.log(`Server running on port ${port}`);
   });
-}
+// }
 
 module.exports = app;
